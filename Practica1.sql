03a1acbadb33061a01261eae7ffb91962ed2f3ed
Boyckott <pum_hsp@hotmail.com>
2016-01-20 21:33:25 -0600

    Update Practica1.sql

--- Practica1.sql
+++ Practica1.sql
@@ -1,1 +1,19 @@
+-- Crearemos una tabla muy simple para probar el poder de PL SQL 
 
+CREATE TABLE DANCE(ID_DANCE INTEGER, NOMBRE VARCHAR2(40), EDAD INTEGER);
+--Prendemos el servidor en modo de desarrolador 
+SET SERVEROUTPUT ON;
+--Vamos a empezar con un bloque PL SQL 
+DECLARE 
+--Aqui se declaran las variables
+EDAD INTEGER;
+YEARS INTEGER;
+BEGIN
+--Aqui la logica de tu programita
+YEARS:=24;
+EDAD:=YEARS*365;
+
+DBMS_OUTPUT.PUT_LINE('TU EDAD EN DIAS ES '||EDAD);
+
+END;
+/
